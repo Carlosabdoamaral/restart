4f0c5166a9e46392643fd65cc23d103bf9f1a5cc
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-11-15 01:13:39 -0300

    adicionando animacoes e afins

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -10,18 +10,133 @@ import SwiftUI
 struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive : Bool = true
+    @State private var buttonWidth : Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset : CGFloat = 0
+    @State private var isAnimating : Bool = false
+    
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("OnBoarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        }
+            VStack(spacing: 20) {
+                //MARK: HEADER
+                Spacer()
+                
+                VStack (spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                    
+                } //: HEADER
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                
+                //MARK: CENTER
+                ZStack {
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                } //: CENTER
+                Spacer()
+                
+                //MARK: FOOTER
+                ZStack {
+                    // Partes do botao
+                    // 1. Background
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. Call-to-action
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .offset(x: 20)
+                    
+                    // 3. Capsule
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: buttonOffset + 80)
+                        Spacer()
+                    }//: HSTACK
+                    
+                    // 4. Circle
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(Color.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size:24, weight: .bold))
+                        }//: ZSTACK
+                        .foregroundColor(Color.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    
+                                    // Só vai fazer algo se o usuário arrastar
+                                    // Vai prevenir do usuário de jogar fora do componente
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                // Vai fazer o botao voltar ao normal quando o usuario soltar
+                                .onEnded { _ in
+                                    
+                                    //Se tiver mais para a direita ele vai preencher e mudar a tela
+                                    //Caso contrario vai ir voltar ao 0
+                                    withAnimation(Animation.easeOut(duration: 0.4)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        } else {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                }
+                        )
+                        
+                        Spacer()
+                    }//: HSTACK
+                    
+                }//: FOOTER
+                .frame(width: buttonWidth, height: 80, alignment: .center)
+                .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+            } // : VSTACK
+            
+        } // : ZSTACK
+        .onAppear(perform: {
+            isAnimating = true
+        })
         
     }
 }
